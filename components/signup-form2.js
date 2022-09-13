83ee3f7648791b01444d33676820b8417f463c3c
Tim Campbell <timcampbell@hey.com>
2022-09-13 09:30:02 -0400

    improve visuals on industry picker

--- components/signup-form2.js
+++ components/signup-form2.js
@@ -50,7 +50,7 @@ export default function SignupForm2() {
             </div>
             <div className="hidden md:block mx-auto w-full max-w-4xl space-y-8">            
               <div className="grid gap-2 md:grid-cols-2 lg:grid-cols-3 ">
-                <div className="bg-white p-4 border border-solid rounded-md hover:bg-gray-50">
+                <div className="bg-white p-4 border border-solid border-l-8 border-l-yellow-500 rounded-md hover:bg-gray-50">
                   <label htmlFor="industry-construction" className="flex cursor-pointer items-center text-lg font-medium leading-6 text-gray-900">
                     <input
                       id="industry-construction"
@@ -58,15 +58,15 @@ export default function SignupForm2() {
                       type="radio"
                       className="h-4 w-4 mr-2 border-gray-300 text-indigo-600 focus:ring-indigo-500"
                     />
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-yellow-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <BuildingOffice2Icon className="h-6 w-6 text-black-600" aria-hidden="true" />
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10">
+                      <BuildingOffice2Icon className="h-8 w-8 text-yellow-500 aria-hidden-true" />
                     </div>
                     <div className="mt-0 ml-2 text-left">
                         Construction
                     </div>
                   </label>
                 </div>            
-                <div className="bg-white p-4 border border-solid rounded-md hover:bg-gray-50">
+                <div className="bg-white p-4 border border-solid border-l-8 border-l-orange-600 rounded-md hover:bg-gray-50">
                   <label htmlFor="industry-energy" className="flex cursor-pointer items-center text-lg font-medium leading-6 text-gray-900">
                     <input
                       id="industry-energy"
@@ -74,15 +74,15 @@ export default function SignupForm2() {
                       type="radio"
                       className="h-4 w-4 mr-2 border-gray-300 text-indigo-600 focus:ring-indigo-500"
                     />
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-orange-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <BoltIcon className="h-6 w-6 text-orange-600" aria-hidden="true" />
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10">
+                      <BoltIcon className="h-8 w-8 text-orange-600 aria-hidden-true" />
                     </div>
                     <div className="mt-0 ml-2 text-left">
                         Energy
                     </div>
                   </label>
                 </div>            
-                <div className="bg-white p-4 border border-solid rounded-md hover:bg-gray-50">
+                <div className="bg-white p-4 border border-solid border-l-8 border-l-green-500 rounded-md hover:bg-gray-50">
                   <label htmlFor="industry-environmental" className="flex cursor-pointer items-center text-lg font-medium leading-6 text-gray-900">
                     <input
                       id="industry-environmental"
@@ -90,15 +90,15 @@ export default function SignupForm2() {
                       type="radio"
                       className="h-4 w-4 mr-2 border-gray-300 text-indigo-600 focus:ring-indigo-500"
                     />
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <GlobeAmericasIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10">
+                      <GlobeAmericasIcon className="h-8 w-8 text-green-500 aria-hidden-true" />
                     </div>
                     <div className="mt-0 ml-2 text-left">
                         Enviromental
                     </div>
                   </label>
                 </div>            
-                <div className="bg-white p-4 border border-solid rounded-md hover:bg-gray-50">
+                <div className="bg-white p-4 border border-solid border-l-8 border-l-sky-800 rounded-md hover:bg-gray-50">
                   <label htmlFor="industry-manufacturing" className="flex cursor-pointer items-center text-lg font-medium leading-6 text-gray-900">
                     <input
                       id="industry-manufacturing"
@@ -106,15 +106,15 @@ export default function SignupForm2() {
                       type="radio"
                       className="h-4 w-4 mr-2 border-gray-300 text-indigo-600 focus:ring-indigo-500"
                     />
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-gray-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <RectangleStackIcon className="h-6 w-6 text-gray-600" aria-hidden="true" />
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10">
+                      <RectangleStackIcon className="h-8 w-8 text-sky-800 aria-hidden-true" />
                     </div>
                     <div className="mt-0 ml-2 text-left">
                         Manufacturing
                     </div>
                   </label>
                 </div>            
-                <div className="bg-white p-4 border border-solid rounded-md hover:bg-gray-50">
+                <div className="bg-white p-4 border border-solid border-l-8 border-l-pink-500 rounded-md hover:bg-gray-50">
                   <label htmlFor="industry-retail" className="flex cursor-pointer items-center text-lg font-medium leading-6 text-gray-900">
                     <input
                       id="industry-retail"
@@ -122,15 +122,15 @@ export default function SignupForm2() {
                       type="radio"
                       className="h-4 w-4 mr-2 border-gray-300 text-indigo-600 focus:ring-indigo-500"
                     />
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-pink-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <BuildingStorefrontIcon className="h-6 w-6 text-pink-600" aria-hidden="true" />
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10">
+                      <BuildingStorefrontIcon className="h-8 w-8 text-pink-500 aria-hidden-true" />
                     </div>
                     <div className="mt-0 ml-2 text-left">
                         Retail
                     </div>
                   </label>
                 </div>            
-                <div className="bg-white p-4 border border-solid rounded-md hover:bg-gray-50">
+                <div className="bg-white p-4 border border-solid border-l-8 border-l-blue-500 rounded-md hover:bg-gray-50">
                   <label htmlFor="industry-telecom" className="flex cursor-pointer items-center text-lg font-medium leading-6 text-gray-900">
                     <input
                       id="industry-telecom"
@@ -138,15 +138,15 @@ export default function SignupForm2() {
                       type="radio"
                       className="h-4 w-4 mr-2 border-gray-300 text-indigo-600 focus:ring-indigo-500"
                     />
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <PhoneIcon className="h-6 w-6 text-blue-600" aria-hidden="true" />
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10">
+                      <PhoneIcon className="h-8 w-8 text-blue-500 aria-hidden-true" />
                     </div>
                     <div className="mt-0 ml-2 text-left">
                         Telecommunications
                     </div>
                   </label>
                 </div>            
-                <div className="bg-white p-4 border border-solid rounded-md hover:bg-gray-50">
+                <div className="bg-white p-4 border border-solid border-l-8 border-l-amber-800 rounded-md hover:bg-gray-50">
                   <label htmlFor="industry-transportation" className="flex cursor-pointer items-center text-lg font-medium leading-6 text-gray-900">
                     <input
                       id="industry-transportation"
@@ -154,15 +154,15 @@ export default function SignupForm2() {
                       type="radio"
                       className="h-4 w-4 mr-2 border-gray-300 text-indigo-600 focus:ring-indigo-500"
                     />
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <TruckIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10">
+                      <TruckIcon className="h-8 w-8 text-amber-800 aria-hidden-true" />
                     </div>
                     <div className="mt-0 ml-2 text-left">
                         Transportation
                     </div>
                   </label>
                 </div>            
-                <div className="bg-white p-4 border border-solid rounded-md hover:bg-gray-50">
+                <div className="bg-white p-4 border border-solid border-l-8 border-l-cyan-500 rounded-md hover:bg-gray-50">
                   <label htmlFor="industry-utilities" className="flex cursor-pointer items-center text-lg font-medium leading-6 text-gray-900">
                     <input
                       id="industry-utilities"
@@ -170,15 +170,15 @@ export default function SignupForm2() {
                       type="radio"
                       className="h-4 w-4 mr-2 border-gray-300 text-indigo-600 focus:ring-indigo-500"
                     />
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-cyan-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <FireIcon className="h-6 w-6 text-cyan-600" aria-hidden="true" />
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10">
+                      <FireIcon className="h-8 w-8 text-cyan-500 aria-hidden-true" />
                     </div>
                     <div className="mt-0 ml-2 text-left">
                         Utilities
                     </div>
                   </label>
                 </div>            
-                <div className="bg-white p-4 border border-solid rounded-md hover:bg-gray-50">
+                <div className="bg-white p-4 border border-solid border-l-8 border-l-stone-500 rounded-md hover:bg-gray-50">
                   <label htmlFor="industry-other" className="flex cursor-pointer items-center text-lg font-medium leading-6 text-gray-900">
                     <input
                       id="industry-other"
@@ -186,8 +186,8 @@ export default function SignupForm2() {
                       type="radio"
                       className="h-4 w-4 mr-2 border-gray-300 text-indigo-600 focus:ring-indigo-500"
                     />
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-stone-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <QuestionMarkCircleIcon className="h-6 w-6 text-stone-600" aria-hidden="true" />
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full sm:mx-0 sm:h-10 sm:w-10">
+                      <QuestionMarkCircleIcon className="h-8 w-8 text-stone-500 aria-hidden-true" />
                     </div>
                     <div className="mt-0 ml-2 text-left">
                         Other
@@ -211,7 +211,7 @@ export default function SignupForm2() {
                     className="group relative flex pr-10 ml-6 w-80 justify-center rounded-md border border-transparent bg-sky-400 py-2 px-4 text-sm font-medium text-white hover:bg-sky-500 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2"
                   >
                     <span className="absolute inset-y-0 right-0 flex items-center pr-3">
-                      <ArrowRightIcon className="h-5 w-5 text-sky-200 group-hover:text-sky-50" aria-hidden="true" />
+                      <ArrowRightIcon className="h-5 w-5 text-sky-200 group-hover:text-sky-50" />
                     </span>
                     Next
                   </button>
